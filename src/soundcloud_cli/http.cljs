92f91080269d5cfa3e16892aa88271e9b7a9481c
Tao Yang <tao.yang@zalando.de>
2015-04-16 19:41:53 +0200

    WIP experimenting implementing http.resquest wrapper

--- src/soundcloud_cli/http.cljs
+++ src/soundcloud_cli/http.cljs
@@ -5,8 +5,47 @@
 
 (def node-http (node/require "http"))
 
+(def node-url (node/require "url"))
+
+(defn chan-or-atom?
+  "Determine if supplied argument is a channel or vector"
+  [arg]
+  (cond
+    (satisfies? cljs.core.async.impl.protocols/Channel arg) ::channel
+    (atom? arg) ::atom
+    :else ::unknown))
+
+(defn- gen-put
+  "Generate a function put data into channel or conj to vector inside an atom"
+  [out]
+  (condp = (chan-or-atom? out)
+    ::channel (fn [data] (go (>! out data)))
+    ::atom    (fn [data] (swap! out conj data))))
+
+(defn- gen-close
+  "Generate a function to close a channel or do nothing for atom"
+  [c]
+  (condp = (chan-or-atom? c)
+    ::channel (fn [] (close! c))
+    ::atom    (fn [] nil)))
+
+(defn- gen-data-cb
+  "Generate callback function for response on data event. Take either atom or chan as argument. If there are multiple output channels/atoms, generated callback will put same data on all of them"
+  [out & others]
+  (let [fns (map gen-put (conj others out))]
+    (fn [data]
+      (doall (map #(apply % data) fns)))))
+
+(defn- gen-end-cb
+  [c & others]
+  (let [fns (map gen-close (conj others c))]
+    (fn []
+      (doseq [f fns]
+        (f)))))
+
+
 (defn get
-  "wrapper of node/http get function, takes target url, returns vector of [error-channel response-channel]"
+  "Wrapper of node/http get function, takes target url, returns vector of [error-channel response-channel]"
   [url]
   (let [err-chan  (chan)
         res-chan  (chan)
